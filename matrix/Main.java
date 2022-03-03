70a575c3fd67e93aedb269d57274d05789001793
chege-kimaru <chege-kimaru@gmail.com>
2022-03-03 18:33:55 +0300

    fix: correct matrix algorithm

--- matrix/Main.java
+++ matrix/Main.java
@@ -1,11 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
+        // int[][] matrix = {
+        // { 1, 2, 3 },
+        // { 4, 5, 6 },
+        // { 7, 8, 9 }
+        // };
+
         int[][] matrix = {
-                { 1, 2, 3, 4, 9 },
-                { 5, 6, 7, 8, 5 },
-                { 9, 10, 11, 12, 4 },
-                { 13, 14, 15, 16, 11 }
+                { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 }
         };
 
         int rowStart = 0;
@@ -13,11 +18,12 @@ public class Main {
         int columnStart = 0;
         int columnEnd = matrix[0].length - 1;
 
-        while (rowStart < rowEnd) {
+        while (rowStart <= rowEnd) {
+
             // print the top side
             int tempRow = rowStart;
             int tempColumn = columnStart;
-            while (tempColumn <= columnEnd) {
+            while (tempColumn <= columnEnd && rowStart <= rowEnd) {
                 System.out.print(matrix[tempRow][tempColumn] + " ");
                 tempColumn++;
             }
@@ -26,7 +32,7 @@ public class Main {
             // print The right side
             tempRow = rowStart;
             tempColumn = columnEnd;
-            while (tempRow <= rowEnd) {
+            while (tempRow <= rowEnd && columnStart <= columnEnd) {
                 System.out.print(matrix[tempRow][tempColumn] + " ");
                 tempRow++;
             }
@@ -35,7 +41,7 @@ public class Main {
             // print the bottom side
             tempRow = rowEnd;
             tempColumn = columnEnd;
-            while (tempColumn >= columnStart) {
+            while (tempColumn >= columnStart && rowStart <= rowEnd) {
                 System.out.print(matrix[tempRow][tempColumn] + " ");
                 tempColumn--;
             }
@@ -44,7 +50,7 @@ public class Main {
             // print the left side
             tempRow = rowEnd;
             tempColumn = columnStart;
-            while (tempRow >= rowStart) {
+            while (tempRow >= rowStart && columnStart <= columnEnd) {
                 System.out.print(matrix[tempRow][tempColumn] + " ");
                 tempRow--;
             }
